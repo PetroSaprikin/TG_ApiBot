d3c340859e28888132537af85bc712f4399fc920
Petro Saprykin <petrosaprikinloh@gmail.com>
2023-05-14 01:03:58 +0300

    Убрал глобальные переменные, добавил команду Cancel

--- handlers/custom_handlers/__init__.py
+++ handlers/custom_handlers/__init__.py
@@ -1,2 +1,2 @@
-from . import low, relevant
+from . import low
 
--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,15 +1,18 @@
 from telebot.types import Message
-from states.low_prices_states import LowPricesState
-from site_api.core import headers, querystring, querystring1, url, url1
-
-from loader import bot
 import requests
 
-params = {}
-headers = headers
-querystring = querystring
-querystring1 = querystring1
-querystring["sort_order"] = "PRICE_LOW_TO_HIGH"
+from states.low_prices_states import LowPricesState
+from site_api.core import headers, querystring, querystring1, url, url1
+from config_data.config import DEFAULT_COMMANDS
+from loader import bot
+
+
+@bot.message_handler(state="*", commands=['cancel'])
+def any_state(message):
+    bot.send_message(message.chat.id, "Выполнение команды отменено!")
+    bot.delete_state(message.from_user.id, message.chat.id)
+    text = [f"/{command} - {desk}" for command, desk in DEFAULT_COMMANDS]
+    bot.reply_to(message, "\n".join(text))
 
 
 @bot.message_handler(commands=["low"])
@@ -21,10 +24,12 @@ def bot_low(message: Message) -> None:
 @bot.message_handler(state=LowPricesState.city)
 def city(message: Message) -> None:
     if message.text.isalpha():
-        params['city'] = message.text
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            data['city'] = message.text
         bot.set_state(message.from_user.id, LowPricesState.checkin_date, message.chat.id)
         bot.send_message(message.chat.id, "Введите дату заселения (в формате гггг-мм-дд)")
         querystring1["query"] = message.text
+        querystring["sort_order"] = "PRICE_LOW_TO_HIGH"
         response = requests.get(url1, headers=headers, params=querystring1)
         querystring["region_id"] = response.json()["data"][0]["gaiaId"]
     else:
@@ -36,7 +41,8 @@ def checkin_date(message: Message):
     check = message.text.split("-")
     if len(check) == 3 and len(check[0]) == 4 and len(check[1]) == 2 and len(check[2]) == 2:
         if 1 < int(check[1]) < 12 and 1 <= int(check[2]) <= 31:
-            params['checkin_date'] = message.text
+            with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+                data["checkin_date"] = message.text
             bot.set_state(message.from_user.id, LowPricesState.checkout_date, message.chat.id)
             bot.send_message(message.chat.id, "Введите дату выселения (в формате гггг-мм-дд)")
             querystring["checkin_date"] = message.text
@@ -49,7 +55,8 @@ def checkout_date(message: Message):
     check = message.text.split("-")
     if len(check) == 3 and len(check[0]) == 4 and len(check[1]) == 2 and len(check[2]) == 2:
         if 1 < int(check[1]) < 12 and 1 <= int(check[2]) <= 31:
-            params['checkout_date'] = message.text
+            with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+                data["checkout_date"] = message.text
             bot.set_state(message.from_user.id, LowPricesState.results_amount, message.chat.id)
             bot.send_message(message.chat.id, "Теперь введите искомое кол-во результатов от 1 до 10ти")
             querystring["checkout_date"] = message.text
@@ -60,9 +67,13 @@ def checkout_date(message: Message):
 @bot.message_handler(state=LowPricesState.results_amount)
 def results(message: Message):
     if message.text.isdigit():
-        bot.send_message(message.chat.id, f"Город: {params['city']}\n"
-                                          f"Дата заселения: {params['checkin_date']}\n"
-                                          f"Дата выселения: {params['checkout_date']}")
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            msg = (
+                f"Город: {data['city']}\n"
+                f"Дата заселения: {data['checkin_date']}\n"
+                f"Дата выселения: {data['checkout_date']}"
+            )
+        bot.send_message(message.chat.id, msg)
         response = requests.get(url, headers=headers, params=querystring)
         if 0 < int(message.text) <= 10:
             for _ in range(int(message.text)):
